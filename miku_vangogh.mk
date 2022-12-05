a6569cd49ba15b913fad0411b3af73f721e56825
ChiwaCime <51258794+ChiwaCime@users.noreply.github.com>
2022-12-05 19:19:20 +0800

    Update and rename aosp_vangogh.mk to miku_vangogh.mk

--- miku_vangogh.mk
+++ miku_vangogh.mk
@@ -8,21 +8,23 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common PixelExperience stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common miku stuff.
+$(call inherit-product, vendor/miku/config/common_full_phone.mk)
 
 # Inherit from vangogh device
 $(call inherit-product, device/xiaomi/vangogh/device.mk)
 
-PRODUCT_NAME := aosp_vangogh
+PRODUCT_NAME := miku_vangogh
 PRODUCT_DEVICE := vangogh
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := M2002J9E
 
 TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_INCLUDE_PIXEL_CHARGER := true
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="vangogh-user 11 RKQ1.200826.002 V12.1.7.0.RJVCNXM release-keys"
