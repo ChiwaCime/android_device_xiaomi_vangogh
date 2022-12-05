14632c526154e7925fc77055a3c0926e53ad7880
ChiwaCime <51258794+ChiwaCime@users.noreply.github.com>
2022-12-05 19:01:01 +0800

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_vangogh.mk
+    $(LOCAL_DIR)/miku_vangogh.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_vangogh-user \
-    aosp_vangogh-userdebug \
-    aosp_vangogh-eng
+    miku_vangogh-user \
+    miku_vangogh-userdebug \
+    miku_vangogh-eng
